63839c703a60ebc7cc1ac4cc54fad5103fce76f2
M1ndBlast <gustavopdzz0@gmail.com>
2019-06-07 21:41:15 -0500

    funcion terminada para agregar app

--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,25 @@
 const express = require('express');
 const router = express.Router();
 const io = require('socket.io-client');
+const path = require('path');
 
 /* GET home page. */
 router.get('/', function(req, res)
 {
-  res.render('index', { title: 'AidChips' });
+    res.render('index', { title: 'AidChips' });
 });
+router.get('/app', function(req, res)
+{
+    res.download(path.join(__dirname, '../public/js/minder.js'));
+});
+
 router.get('/s/*', function(req, res)
 {
-  let socket = io.connect('https://'+req.get('host'), { forceNew: true });
-  let idStr = req.originalUrl.substring(req.originalUrl.lastIndexOf("/")+1, req.originalUrl.length);
-  socket.emit('sendingData', idStr, true);
+    let socket = io.connect('https://'+req.get('host'), { forceNew: true });
+    let idStr = req.originalUrl.substring(req.originalUrl.lastIndexOf("/")+1, req.originalUrl.length);
+    socket.emit('sendingData', idStr, true);
 
-  res.send(true)
+    res.send(true)
 });
 
 module.exports = router;
